135a81ff412eb8948870692fd5ce45df591854cb
arturovilla <acvturi@gmail.com>
2023-03-24 12:42:55 -0500

    adust image size

--- components/Card.js
+++ components/Card.js
@@ -6,7 +6,7 @@ function Card({title,pic}) {
     <a href="#" className="text-center p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
         <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{title.text}</h5>
         <div style={{display: "flex",justifyContent: "center",}}>
-            <Image src={pic} width="100" height="1000" />
+            <Image src={pic} width="100" height="20" />
         </div>
     </a>
   )
